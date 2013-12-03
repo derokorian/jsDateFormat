66836753f450f265640c980f1ddef128ad39298c
Ryan Pallas <ryan.pallas@gmail.com>
2013-12-02 21:21:47 -0700

    Cleaned up to pass JSLint

--- Date.prototype.format.js
+++ Date.prototype.format.js
@@ -7,7 +7,7 @@
  * @returns string The formatted date string
  *
  * @author Ryan Pallas <ryan.pallas (at) gmail.com>
- * @version 0.1.0
+ * @version 1.0.0
  * @license The MIT License (MIT)
 
  * @Copyright (c)2013 Ryan Pallas <ryan.pallas (at) gmail.com>
@@ -59,23 +59,32 @@
  *    - Add Localizations
  *    - More options
  */
-Date.prototype.format = function(strFormat,strL18n) {
-    strL18n = typeof strL18n == 'undefined' ? 'en' : strL18n;
+Date.prototype.format = function (strFormat, strL18n) {
+    "use strict";
+    strL18n = (strL18n === undefined) ? 'en' : strL18n;
     var aMonths = {
-            'en' : ['January','February','March','April','May',
-                'June','July','August','September','October',
-                'November','December']
+            'en' : ['January', 'February', 'March', 'April', 'May',
+                'June', 'July', 'August', 'September', 'October',
+                'November', 'December']
         },
         aDays = {
-            'en' : ['Sunday','Monday','Tuesday','Wednesday',
-                'Thursday','Friday','Saturday']
+            'en' : ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+                'Thursday', 'Friday', 'Saturday']
         },
         strRetVal = '',
         chPrev = '',
-        iCharCount = 0;
-    for( var i = 0; i <= strFormat.length; i++ )
+        iCharCount = 0,
+        i,
+        iMonth = this.getMonth() + 1,
+        iDate = this.getDate(),
+        iDay = this.getDay(),
+        iHrs = this.getHours(),
+        i12Hrs = (iHrs + 11) % 12 + 1,
+        iMin = this.getMinutes(),
+        iSec = this.getSeconds();
+    for ( i = 0; i <= strFormat.length; i++)
     {
-        if( strFormat.charAt(i) == chPrev ) 
+        if (strFormat.charAt(i) == chPrev) 
         {
             iCharCount++;
         }
@@ -96,61 +105,58 @@ Date.prototype.format = function(strFormat,strL18n) {
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join(chPrev);
+                            strRetVal += new Array(iCharCount + 1).join(chPrev);
                         }
                         break;
                     case 'm':
                         if (iCharCount == 2)
                         {
-                            var iMonth = this.getMonth() + 1;
                             strRetVal += (iMonth < 10 ? '0' : '') + iMonth;
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += this.getMonth();
+                            strRetVal += iMonth;
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('m');
+                            strRetVal += new Array(iCharCount + 1).join('m');
                         }
                         break;
                     case 'M':
                         if (iCharCount == 2)
                         {
-                            strRetVal += aMonths[strL18n][this.getMonth()];
+                            strRetVal += aMonths[strL18n][iMonth - 1];
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += aMonths[strL18n][this.getMonth()].substring(0,3);
+                            strRetVal += aMonths[strL18n][iMonth - 1].substring(0,3);
                         }
                         else
                         {
-                            strRetVal += Array(iCharCont + 1).join('M');
+                            strRetVal += new Array(iCharCount + 1).join('M');
                         }
                         break;
                     case 'd': 
                         if (iCharCount == 2)
                         {
-                            var iDay = this.getDate();
-                            strRetVal += (iDay < 10 ? '0' : '') + iDay;
+                            strRetVal += (iDate < 10 ? '0' : '') + iDate;
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += this.getDate();
+                            strRetVal += iDate;
                         }  
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('d');
+                            strRetVal += new Array(iCharCount + 1).join('d');
                         }
                         break;
                     case 'D':
                         if (iCharCount == 2)
                         {
-                            strRetVal += aDays[this.getDay()];
+                            strRetVal += aDays[strL18n][iDay];
                         }
                         else if (iCharCount == 1)
                         {
-                            var iDay = this.getDay();
                             if ( strL18n == 'en' )
                             {
                                 if ( [0,2,4,6].indexOf(iDay) > -1 )
@@ -165,91 +171,87 @@ Date.prototype.format = function(strFormat,strL18n) {
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('D');
+                            strRetVal += new Array(iCharCount + 1).join('D');
                         }
                         break;
                     case 'H':
                         if (iCharCount == 2)
                         {
-                            var iHrs = this.getHours();
                             strRetVal += (iHrs < 10 ? '0' : '') + iHrs;
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += this.getHours();
+                            strRetVal += iHrs;
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('H');
+                            strRetVal += new Array(iCharCount + 1).join('H');
                         }
                         break;
                     case 'h':
                         if (iCharCount == 2)
                         {
-                            var iHrs = ((this.getHours() + 11) % 12 + 1);
-                            strRetVal += (iHrs < 10 ? '0' : '') + iHrs;                            
+                            strRetVal += (i12Hrs < 10 ? '0' : '') + i12Hrs;                            
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += ((this.getHours() + 11) % 12 + 1);
+                            strRetVal += i12Hrs;
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('h');
+                            strRetVal += new Array(iCharCount + 1).join('h');
                         }
                         break;
                     case 'n':
                         if (iCharCount == 2)
                         {
-                            var iMin = this.getMinutes();
                             strRetVal += (iMin < 10 ? '0' : '') + iMin;
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal += this.getMinutes();
+                            strRetVal += iMin;
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('n');
+                            strRetVal += new Array(iCharCount + 1).join('n');
                         }
                         break;
                     case 's':
                         if (iCharCount == 2)
                         {
-                            var iSec = this.getSeconds();
                             strRetVal += (iSec < 10 ? '0' : '') + iSec;
                         }
                         else if (iCharCount == 1)
                         {
-                            strRetVal +=  this.getSeconds();
+                            strRetVal +=  iSec;
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('s');
+                            strRetVal += new Array(iCharCount + 1).join('s');
                         }
                         break;
                     case 'j':
                         if (iCharCount == 1)
                         {
-                            strRetVal += this.getHours() > 0 && this.getHours() < 13 ? 'am' : 'pm';
+                            strRetVal += iHrs > 0 && iHrs < 13 ? 'am' : 'pm';
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('j');
+                            strRetVal += new Array(iCharCount + 1).join('j');
                         }
                         break;
                     case 'J':
                         if (iCharCount == 1)
                         {
-                            strRetVal += this.getHours() > 0 && this.getHours() < 13 ? 'AM' : 'PM';
+                            strRetVal += iHrs > 0 && iHrs < 13 ? 'AM' : 'PM';
                         }
                         else
                         {
-                            strRetVal += Array(iCharCount + 1).join('J');
+                            strRetVal += new Array(iCharCount + 1).join('J');
                         }
                         break;
                     default:
-                        strRetVal += Array(iCharCount + 1).join(chPrev);
+                        strRetVal += new Array(iCharCount + 1).join(chPrev);
                 }
             }
             chPrev = strFormat.charAt(i);
