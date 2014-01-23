6910564aca8031785b07a827a9ec773021494b9c
Ryan Pallas <ryan.pallas@gmail.com>
2014-01-22 20:40:37 -0700

    CLI tests

--- tests/cli/jsDateSpec.js
+++ tests/cli/jsDateSpec.js
@@ -9,10 +9,11 @@ describe('jsDate built-ins', function() {
         }
     });
     
-    it('has properties and methods', function() {
+    it('exists', function(){
         expect(Date.prototype.jsDateFormat).toBeDefined();
         expect(typeof Date.prototype.jsDateFormat).toEqual('object');
-        
+    });
+    it('has properties and methods', function() {
         expect(Date.prototype.jsDateFormat.localizations).toBeDefined();
         expect(typeof Date.prototype.jsDateFormat.localizations).toEqual('object');
         
@@ -21,5 +22,20 @@ describe('jsDate built-ins', function() {
         
         expect(Date.prototype.jsDateFormat.getTZ).toBeDefined();
         expect(typeof Date.prototype.jsDateFormat.getTZ).toEqual('function');
+    });
+    
+    it('extends Date', function() {
+        expect(Date.prototype.format).toBeDefined();
+        expect(typeof Date.prototype.format).toEqual('function');
+        
+        expect(Date.parseFormat).toBeDefined();
+        expect(typeof Date.parseFormat).toEqual('function');
+    });
+    
+    it('parses timezones correctly', function() {
+        var func = Date.prototype.jsDateFormat.getTZ;
+        expect(func(0)).toEqual('+0000');
+        expect(func(420)).toEqual('-0700');
+        expect(func(-60)).toEqual('+0100');
     });
 });
