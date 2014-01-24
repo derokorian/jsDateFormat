49937fa73b2354e10811edb4fb2c5cad148bf45d
Ryan Pallas <ryan.pallas@gmail.com>
2014-01-23 20:02:52 -0700

    Test fixes

--- tests/cli/jsDateSpec.js
+++ tests/cli/jsDateSpec.js
@@ -1,5 +1,4 @@
-describe('jsDate built-ins', function() {
-    
+describe('jsDateFormat', function() {
     beforeEach(function() {
         if( typeof Date.prototype.jsDateFormat == 'undefined' ) {
             fs = require('fs');
@@ -13,23 +12,37 @@ describe('jsDate built-ins', function() {
         expect(Date.prototype.jsDateFormat).toBeDefined();
         expect(typeof Date.prototype.jsDateFormat).toEqual('object');
     });
-    it('has properties and methods', function() {
-        expect(Date.prototype.jsDateFormat.localizations).toBeDefined();
-        expect(typeof Date.prototype.jsDateFormat.localizations).toEqual('object');
+    
+    describe('has properties and methods', function() {
+        it('localizations', function() {
+            expect(Date.prototype.jsDateFormat.localizations).toBeDefined();
+            expect(typeof Date.prototype.jsDateFormat.localizations).toEqual('object');
+        });
         
-        expect(Date.prototype.jsDateFormat.setLocalization).toBeDefined();
-        expect(typeof Date.prototype.jsDateFormat.setLocalization).toEqual('function');
+        it('setLocalization', function() {
+            expect(Date.prototype.jsDateFormat.setLocalization).toBeDefined();
+            expect(typeof Date.prototype.jsDateFormat.setLocalization).toEqual('function');
+        });
         
-        expect(Date.prototype.jsDateFormat.getTZ).toBeDefined();
-        expect(typeof Date.prototype.jsDateFormat.getTZ).toEqual('function');
+        it('getTZ', function() {
+            expect(Date.prototype.jsDateFormat.getTZ).toBeDefined();
+            expect(typeof Date.prototype.jsDateFormat.getTZ).toEqual('function');
+        });
     });
     
-    it('extends Date', function() {
-        expect(Date.prototype.format).toBeDefined();
-        expect(typeof Date.prototype.format).toEqual('function');
-        
-        expect(Date.parseFormat).toBeDefined();
-        expect(typeof Date.parseFormat).toEqual('function');
+    describe('extends Date', function() {
+        it('with format', function() {
+            expect(Date.prototype.format).toBeDefined();
+            expect(typeof Date.prototype.format).toEqual('function');
+        });
+        it('with toFormat', function() {
+            expect(Date.prototype.toFormat).toBeDefined();
+            expect(typeof Date.prototype.toFormat).toEqual('function');
+        });
+        it('with fromFormat', function() {
+            expect(Date.fromFormat).toBeDefined();
+            expect(typeof Date.fromFormat).toEqual('function');
+        });
     });
     
     it('parses timezones correctly', function() {
